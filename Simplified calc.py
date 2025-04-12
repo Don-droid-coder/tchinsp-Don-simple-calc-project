99921b4b1ddcca633b40c52f846fde88f73c1e1a
Don-droid-coder <dondarcsh@gmail.com>
2025-04-12 00:40:03 -0700

    Update Simplified calc.py
    Minor errors resolved

--- Simplified calc.py
+++ Simplified calc.py
@@ -1,4 +1,18 @@
-txt = "The best things in life are free!"
-if "free" in txt :
-    print("Yes")
-
+print("Hello, there!")
+print("Enter any two numbers to be calculated")
+x = float(input("What is the first number?: "))
+y = float(input("What's the second number?: "))
+z = (input("What's the operation (+, -, *, /): 20"))
+operation = f"Your request is {x} {z} {y}"
+print(operation)
+if z == "+" :
+    result_calc = x + y
+elif z == "-" :
+    result_calc = x - y
+elif z == "*" :
+    result_calc = x * y
+elif z == "/" :
+    result_calc = x / y     
+  
+result = f"The results are {result_calc}"
+print(result)
